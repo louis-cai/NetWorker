d23165433a1b2c139ae9354635a510fee2cb6cf0
cailu <louis.cai.cn@gmail.com>
2015-03-10 10:18:16 +0800

    1、新版本1.0.0 2、清除example中的pods管理

--- Example/Pods/Target Support Files/Pods-NetWorker-AFNetworkActivityLogger/Pods-NetWorker-AFNetworkActivityLogger-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_NetWorker_AFNetworkActivityLogger : NSObject
-@end
-@implementation PodsDummy_Pods_NetWorker_AFNetworkActivityLogger
-@end
